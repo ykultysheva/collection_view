4914aa80e88c1a45de91b299372d43724660767c
James N. V. Cash <james.nvc@gmail.com>
2016-07-13 09:19:33 -0400

    Initial Commit

---
+++ CollectionViewsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewsDemo
+//
+//  Created by James Cash on 13-07-16.
+//  Copyright © 2016 Occasionally Cogent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionViewsDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CollectionViewsDemo
+//
+//  Created by James Cash on 13-07-16.
+//  Copyright © 2016 Occasionally Cogent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
